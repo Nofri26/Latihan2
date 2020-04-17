47e7066731f2477cde8e5e05cfee974a7ac7f99f
Nofrizal <1zhal9991@gmail.com>
2020-04-18 00:18:53 +0700

    Menambah Data diri

--- app/src/main/java/com/squidward/latihan2/MainActivity.kt
+++ app/src/main/java/com/squidward/latihan2/MainActivity.kt
@@ -4,7 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-
+/*
+* NIM   : 10117139
+* Nama  : Nofrizal
+* Kelas : AKB-4 / IF-4
+* Tugas : AKB Latihan 2
+*
+* Update :
+* - 16 April =
+*       - Membuat Halaman 1 & 2
+*       - Upload Github
+* - 17 april =
+*       - Membuat halaman 3 - 5
+*       - validasi dll
+*
+* */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
